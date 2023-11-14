97f0e8e3083093138eab6067d76f4a2a8110a41f
m e s s <erald.muhammad@gmail.com>
2023-11-14 14:04:35 +0700

    adding development and production state

--- backend/python-fastapi/main.py
+++ backend/python-fastapi/main.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 from generate_jwt import get_jwt
@@ -20,3 +25,14 @@ def get_token(username):
         response = {"status": "error", "messages": str(e)}
     finally:
         return response
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    PORT = int(os.getenv("PORT", 3000))
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "dev":
+            uvicorn.run("main:app", port=PORT, log_level="debug", reload=True)
+    else:
+        uvicorn.run("main:app", port=PORT, log_level="info", reload=False)
