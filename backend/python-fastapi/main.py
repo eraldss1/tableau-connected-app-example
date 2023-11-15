a2db6db5dbd7e7667a8135ec6de695702f5c6f44
eraldss1 <erald.muhammad@gmail.com>
2023-11-15 11:02:15 +0700

    add CORS

--- backend/python-fastapi/main.py
+++ backend/python-fastapi/main.py
@@ -4,10 +4,21 @@ import sys
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from generate_jwt import get_jwt
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
