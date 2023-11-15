ba5a916f13776be89ddde225c11a7ebc05fc9bee
eraldss1 <erald.muhammad@gmail.com>
2023-11-15 10:33:24 +0700

    get port from env

--- backend/node.js-express/app.js
+++ backend/node.js-express/app.js
@@ -1,15 +1,16 @@
 const { loadRoute } = require("./src/route");
 
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
 
-app.use(cors())
+app.use(cors());
 loadRoute(app);
 
-app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API is running on port ${PORT}`);
 });
